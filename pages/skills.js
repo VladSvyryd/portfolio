acdcdb09c40f70c84c0d43ced5ea921152d84e49
vladonius <vladonius@gmail.com>
2020-02-19 22:31:43 +0100

    add skills animation

--- pages/skills.js
+++ pages/skills.js
@@ -1,16 +1,35 @@
 import { Nav } from "../components/nav/Nav";
+import { motion } from "framer-motion";
 
 const skills = () => {
   return (
     <>
-      <div className="twoCol">
+      <motion.div
+        exit={{ opacity: 0 }}
+        initial={{ opacity: 0, translateX: "+100%" }}
+        animate={{ opacity: 1, translateX: "0%" }}
+        className="twoCol"
+      >
         <div>
           <Nav />
         </div>
-        <div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            width: "300px"
+          }}
+        >
           <div>This is Skills</div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            sapiente minima itaque qui at illum aperiam praesentium iure enim
+            similique cupiditate odio, ab fugiat fuga dignissimos amet
+            voluptatum ea ad.
+          </p>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
