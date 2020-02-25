82a959a2f79dd4d0f831b1f06e6f5b0a847a56ec
vladonius <vladonius@gmail.com>
2020-02-25 12:45:26 +0100

    clear form after success

--- components/form/form.js
+++ components/form/form.js
@@ -32,7 +32,8 @@ const form = () => {
   const callback = () => {
     toggleOpen();
   };
-  const handleSubmit = async (values, setSubmitting) => {
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    console.log(arguments);
     values = {
       ...values,
       replyTo: `@${values.email}`,
@@ -40,7 +41,6 @@ const form = () => {
       accessKey: "a1b4756a-145c-4d64-8313-cbb68c921082"
     };
     values = { ...values, replyTo: `@${values.email}` };
-    console.log(values);
     try {
       const res = await fetch("https://api.staticforms.xyz/submit", {
         method: "POST",
@@ -55,6 +55,7 @@ const form = () => {
           type: "Success",
           message: "Thank you for reaching out to me."
         });
+        resetForm({});
         setSubmitting(false);
         toggleOpen();
       } else {
@@ -136,9 +137,7 @@ const form = () => {
     >
       <Formik
         initialValues={{ email: "", name: "", subject: "", message: "" }}
-        onSubmit={(values, { setSubmitting }) =>
-          handleSubmit(values, setSubmitting)
-        }
+        onSubmit={handleSubmit}
       >
         {({ isSubmitting }) => (
           <Form className={f.form}>
@@ -303,7 +302,7 @@ const form = () => {
                   width="60px"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <g fill="#f44336">
+                  <g fill="#272641">
                     <path d="m356.339844 296.347656-286.613282-286.613281c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503906-12.5 32.769532 0 45.25l286.613281 286.613282c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082032c12.523438-12.480468 12.523438-32.75.019532-45.25zm0 0" />
                     <path d="m295.988281 9.734375-286.613281 286.613281c-12.5 12.5-12.5 32.769532 0 45.25l15.082031 15.082032c12.503907 12.5 32.769531 12.5 45.25 0l286.632813-286.59375c12.503906-12.5 12.503906-32.765626 0-45.246094l-15.082032-15.082032c-12.5-12.523437-32.765624-12.523437-45.269531-.023437zm0 0" />
                   </g>
