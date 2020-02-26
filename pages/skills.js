8b1ff4eb676b5dd9469f4364b24f3d06d13e60b6
vladonius <vladonius@gmail.com>
2020-02-26 22:45:36 +0100

    fixed skills style

--- pages/skills.js
+++ pages/skills.js
@@ -8,7 +8,8 @@ const anim_finish_Desktop = {
   opacity: 1,
   translateX: "0%",
   transition: {
-    duration: 1
+    duration: 0.5,
+    type: "spring"
   }
 };
 const container = {
@@ -31,109 +32,100 @@ const item = {
 
 const skills = () => {
   return (
-    <>
-      <motion.div
-        exit={{ opacity: 0, scale: 0 }}
-        className="twoCol "
-        initial={anim_init_Desktop}
-        animate={anim_finish_Desktop}
+    <motion.div
+      exit={{ opacity: 0, scale: 0 }}
+      className="twoCol "
+      initial={anim_init_Desktop}
+      animate={anim_finish_Desktop}
+    >
+      <div className="homeColumn skills ">
+        <h1 aria-label="Skills" className="headline">
+          Skills
+        </h1>
+        <div className="textContainer">
+          <p>
+            Front End developer with experience building responsive websites and
+            web applications.
+          </p>
+          <p>
+            I specialize in JavaScript (TypeScript) and have experience working
+            with React.js and Angular. On the client side I am proficient in
+            HTML, CSS, SASS and popular frameworks: bootstrap, semantic-ui,
+            Angular Material, Onsen UI. My development is reinforced by basic
+            knowledge of software version control system Git.
+          </p>
+          <p>
+            I have also full-stack experience as small Projects on Back End
+            working with Node.js, SQL.
+          </p>
+          <p>
+            Visit my{" "}
+            <a
+              href="https://www.linkedin.com/in/vlad-s-1b2112a6/"
+              target="_blank"
+              className="externalLink"
+            >
+              LinkedIn
+            </a>{" "}
+            profile for more details or{" "}
+            <a href="/contact" className="externalLink">
+              contact
+            </a>{" "}
+            me.
+          </p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
       >
-        <div className="homeColumn skills ">
-          <h1 aria-label="Skills" className="headline">
-            Skills
-          </h1>
-          <div className="textContainer">
-            <p>
-              Front End developer with experience building responsive websites
-              and web applications.
-            </p>
-            <p>
-              I specialize in JavaScript (TypeScript) and have experience
-              working with React.js and Angular. On the client side I am
-              proficient in HTML, CSS, SASS and popular frameworks: bootstrap,
-              semantic-ui, Angular Material, Onsen UI. My development is
-              reinforced by basic knowledge of software version control system
-              Git.
-            </p>
-            <p>
-              I have also full-stack experience as small Projects on Back End
-              working with Node.js, SQL.
-            </p>
-            <p>
-              Visit my{" "}
-              <a
-                href="https://www.linkedin.com/in/vlad-s-1b2112a6/"
-                target="_blank"
-                className="externalLink"
-              >
-                LinkedIn
-              </a>{" "}
-              profile for more details or{" "}
-              <a href="/contact" className="externalLink">
-                contact
-              </a>{" "}
-              me.
-            </p>
-          </div>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            position: "relative"
-          }}
+        <motion.div
+          variants={container}
+          className={` ${skill.grid}`}
+          initial="hidden"
+          animate="visible"
         >
+          <motion.div variants={item} className={`${skill.js} ${skill.icon}`} />
           <motion.div
-            variants={container}
-            className={` ${skill.grid}`}
-            initial="hidden"
-            animate="visible"
-          >
-            <motion.div
-              variants={item}
-              className={`${skill.js} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.css} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.html} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.next} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.react} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.angular} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.jQuery} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.electron} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.sass} ${skill.icon}`}
-            />
-            <motion.div
-              variants={item}
-              className={`${skill.ts} ${skill.icon}`}
-            />
-          </motion.div>
-        </div>
-      </motion.div>
-    </>
+            variants={item}
+            className={`${skill.css} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.html} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.next} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.react} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.angular} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.jQuery} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.electron} ${skill.icon}`}
+          />
+          <motion.div
+            variants={item}
+            className={`${skill.sass} ${skill.icon}`}
+          />
+          <motion.div variants={item} className={`${skill.ts} ${skill.icon}`} />
+        </motion.div>
+      </div>
+    </motion.div>
   );
 };
 export default skills;
