2757acfdb035848d303235b33cb6963b3c5ccbcc
Vlad Svyrydonov <vladonius@gmail.com>
2020-03-25 19:17:24 +0100

    add manifest

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-module.exports = {
+const withOffline = require("next-offline");
+const nextConfig = {
   devIndicators: {
     autoPrerender: false
   }
 };
+
+module.exports = withOffline(nextConfig);
