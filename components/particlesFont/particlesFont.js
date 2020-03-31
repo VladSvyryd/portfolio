8d4161bafc82bdea68c310cc936b311688d11852
Vlad Svyrydonov <vladonius@gmail.com>
2020-03-31 17:12:16 +0200

    add gravity to about p5

--- components/particlesFont/particlesFont.js
+++ components/particlesFont/particlesFont.js
@@ -7,16 +7,18 @@ export default class ParticlesFont extends Component {
   width = this.props.width;
   height = this.props.height;
   vehicles = [];
+  gravityOn = false;
+  gravity;
+  mytext;
   preload = p5 => {
     p5.font = p5.loadFont("./AvenirNextLTPro-Demi.otf");
   };
   setup = (p5, canvasParentRef) => {
-    console.log(this.props);
-
     // p5.createCanvas(500, 500).parent(canvasParentRef); // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
     p5.createCanvas(this.width, this.height).parent(canvasParentRef);
     p5.background(39, 38, 65);
     p5.textFont(p5.font);
+
     var points = p5.font.textToPoints(
       "JS HTML CSS",
       this.width / 5.5,
@@ -28,23 +30,31 @@ export default class ParticlesFont extends Component {
       }
     );
 
-    points.forEach(point => {
-      let vehicle = new Vehicle(point.x, point.y);
-      this.vehicles.push(vehicle);
-      // p5.stroke(108, 99, 255);
-      // p5.strokeWeight(4);
-      // p5.point(p.x, p.y);
-    });
-    function Vehicle(x, y) {
-      this.pos = p5.createVector(p5.random(p5.width), p5.random(p5.height));
-      this.target = p5.createVector(x, y);
-      this.vel = p5.createVector(p5.random(3), p5.random(3));
-      this.acc = p5.createVector();
-      this.r = 3;
-      this.maxSpeed = 10;
-      this.maxforce = 1;
+    class Vehicle {
+      constructor(x, y) {
+        this.pos = p5.createVector(p5.random(p5.width), p5.random(p5.height));
+        this.target = p5.createVector(x, y);
+        this.vel = p5.createVector(p5.random(3), p5.random(3));
+        this.acc = p5.createVector();
+        this.r = 3;
+        this.maxSpeed = 10;
+        this.maxforce = 1;
+      }
 
-      this.behaviors = function() {
+      colideWithEdges = function() {
+        if (this.pos.y >= p5.height) {
+          this.pos.y = p5.height - this.r / 2;
+          this.vel.y *= -0.5;
+        }
+        if (this.pos.x >= p5.width) {
+          this.pos.x = p5.width;
+          this.vel.x *= -0.5;
+        } else if (this.pos.x <= 0) {
+          this.pos.x = 0;
+          this.vel.x *= -0.5;
+        }
+      };
+      behaviors = function() {
         let arrive = this.arrive(this.target);
         let mouse = p5.createVector(p5.mouseX, p5.mouseY);
         let flee = this.flee(mouse);
@@ -53,7 +63,7 @@ export default class ParticlesFont extends Component {
         this.applyForce(arrive);
         this.applyForce(flee);
       };
-      this.flee = function(target) {
+      flee = function(target) {
         let desired = Vector.sub(target, this.pos);
         if (desired.mag() < 50) {
           desired.setMag(this.maxSpeed);
@@ -65,10 +75,10 @@ export default class ParticlesFont extends Component {
           return p5.createVector(0, 0);
         }
       };
-      this.applyForce = function(f) {
+      applyForce = function(f) {
         this.acc.add(f);
       };
-      this.arrive = function(target) {
+      arrive = function(target) {
         let desired = Vector.sub(target, this.pos);
         let distance = desired.mag();
         let speed = this.maxSpeed;
@@ -80,26 +90,73 @@ export default class ParticlesFont extends Component {
         steer.limit(this.maxforce);
         return steer;
       };
-      this.update = function() {
+      update = function() {
         this.pos.add(this.vel);
         this.vel.add(this.acc);
         this.acc.mult(0); // clear force
       };
-      this.show = function() {
+      show = function() {
         p5.stroke(108, 99, 255);
         p5.strokeWeight(this.r);
         p5.point(this.pos.x, this.pos.y);
       };
     }
+    points.forEach(point => {
+      let vehicle = new Vehicle(point.x, point.y);
+      this.vehicles.push(vehicle);
+    });
+
+    class Text extends Vehicle {
+      constructor(x, y, stringText) {
+        super(x, y);
+        this.pos = p5.createVector(x, y);
+        this.textSize = p5.textSize(22);
+        this.text = stringText;
+      }
+
+      show = () => {
+        p5.textAlign(p5.CENTER, p5.CENTER);
+        p5.text(
+          `${this.text ? this.text : "Good luck"}`,
+          this.pos.x,
+          this.pos.y
+        );
+      };
+    }
+    points.forEach(point => {
+      let vehicle = new Vehicle(point.x, point.y);
+      this.vehicles.push(vehicle);
+      // p5.stroke(108, 99, 255);
+      // p5.strokeWeight(4);
+      // p5.point(p.x, p.y);
+    });
+    this.myText = new Text(p5.width / 2, p5.height / 4, "GRAVITY: Off (Space)");
   };
   draw = p5 => {
-    //p5.background(51);
-    // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
     p5.background(39, 38, 65);
+
+    this.myText.update();
+    this.myText.colideWithEdges();
+    this.myText.show();
+    if (this.gravityOn) {
+      let gravity = p5.createVector(0, p5.random(0.6));
+      this.myText.applyForce(gravity);
+      this.myText.text = "GRAVITY: ON (Space)";
+    } else {
+      this.myText.behaviors();
+      this.myText.text = "GRAVITY: OFF (Space)";
+    }
+
     this.vehicles.forEach(vehicle => {
       vehicle.update();
+      vehicle.colideWithEdges();
       vehicle.show();
-      vehicle.behaviors();
+      if (this.gravityOn) {
+        let gravity = p5.createVector(p5.random(-0.35, 0.35), p5.random(1));
+        vehicle.applyForce(gravity);
+      } else {
+        vehicle.behaviors();
+      }
     });
   };
   state = { resized: true };
@@ -108,6 +165,13 @@ export default class ParticlesFont extends Component {
       resized: true
     });
   };
+  handlePress = p5 => {
+    console.log(p5.keyCode);
+    if (p5.keyCode === 32) {
+      this.gravityOn = !this.gravityOn;
+    }
+    console.log(this.gravityOn);
+  };
   render() {
     return this.state.resized ? (
       <Sketch
@@ -115,6 +179,7 @@ export default class ParticlesFont extends Component {
         draw={this.draw}
         preload={this.preload}
         windowResized={this.handleResite}
+        keyReleased={this.handlePress}
       />
     ) : (
       <div>Loading..</div>
