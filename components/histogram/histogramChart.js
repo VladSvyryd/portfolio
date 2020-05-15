239b8a2460e49a2a33da206efbe5cff3c55e6da5
Vlad Svyrydonov <vladonius@gmail.com>
2020-05-15 22:30:55 +0200

    extend works canvas

--- components/histogram/histogramChart.js
+++ components/histogram/histogramChart.js
@@ -24,22 +24,25 @@ export default (props) => {
     setDates(formatDates)
   }, [])
   return (
+    <>
+      <p>{`${props.timestamp.from[0] <= 9 ? 0 : ""}${props.timestamp.from[0]}.${props.timestamp.from[1]} - ${props.timestamp.till[0] <= 9 ? 0 : ""}${props.timestamp.till[0]}.${props.timestamp.till[1]} (${props.timestamp.sum[0]} ${props.timestamp.sum[1]})`}</p>
 
-    <ResponsiveCalendarCanvas
-      data={dates}
-      id={props.id}
-      className={props.className}
-      from={`20${props.timestamp.from[1]}-${props.timestamp.from[0]}-01`}
-      to={`20${props.timestamp.till[1]}-${props.timestamp.till[0]}-01`}
-      emptyColor="#eeeeee"
-      colors={['#f47560']}
-      margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-      yearSpacing={40}
-      monthBorderColor="#ffffff"
-      dayBorderWidth={2}
-      dayBorderColor="#ffffff"
-      labelTextColor={{ theme: 'background' }}
+      <ResponsiveCalendarCanvas
+        data={dates}
+        id={props.id}
+        className={props.className}
+        from={`20${props.timestamp.from[1]}-${props.timestamp.from[0]}-01`}
+        to={`20${props.timestamp.till[1]}-${props.timestamp.till[0]}-01`}
+        emptyColor="#eeeeee"
+        colors={['#f47560']}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        dayBorderColor="#ffffff"
+        labelTextColor={{ theme: 'background' }}
 
-    />
+      />
+    </>
   );
 };
